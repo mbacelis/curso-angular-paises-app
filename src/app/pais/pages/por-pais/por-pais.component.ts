f9d5d7c059fb95e47cbbe782cd2eb353333a7141
Mauricio Bacelis <mbacelis_@hotmail.com>
2022-07-13 13:28:31 -0500

    Se agrega caja de sugerencias a la busqueda

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -4,17 +4,25 @@ import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html'
+  templateUrl: './por-pais.component.html',
+  styles: [`
+    li{
+      cursor: pointer;
+    }
+  `]
 })
 export class PorPaisComponent {
 
   termino: string = "";
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar(termino: string) {
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino;
 
@@ -31,6 +39,20 @@ export class PorPaisComponent {
 
   sugerencias(termino: string) {
     this.hayError = false;
-    // TODO: crear sugerencias
+    this.termino = termino;
+    if (termino === "") {
+      this.mostrarSugerencias = false;
+    } else {
+      this.mostrarSugerencias = true;
+    }
+    this.paisService.buscarPais(termino)
+      .subscribe(
+        paises => this.paisesSugeridos = paises.splice(0, 5),
+        err => this.paisesSugeridos = []
+      );
+  }
+
+  buscarSugerido(termino: string) { 
+    this.buscar(termino);
   }
 }
